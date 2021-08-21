7fe2aa651ecd39c5bae23dbe85b6cbb09fa347af
asimaries <logxtech59@gmail.com>
2021-08-21 07:01:42 -0700

    CYCLE sorT

--- Sorting.h
+++ Sorting.h
@@ -186,6 +186,49 @@ void wavesort(vector<int> &arr)
     }
 }
 
+// CYCLE SORT
+void cycleSort(vector<int> &arr, int n)
+{
+    for (int cs = 0; cs < n - 1; cs++)
+    {
+        int item = arr[cs], pos = cs;
+        for (int i = cs + 1; i < n; i++)
+        {
+            if (arr[i] < item)
+            {
+                pos++;
+            }
+        }
+        if (pos == cs)
+        {
+            continue;
+        }
+        while (item == arr[pos])
+        {
+            pos += 1;
+        }
+        if (pos != cs)
+        {
+            // write++;
+            swap(item, arr[pos]);
+        }
+
+        while (pos != cs)
+        {
+            pos = cs;
+            for (int i = cs + 1; i < n; i++)
+            {
+                if (arr[i] < item)
+                {
+                    pos++;
+                }
+                swap(item, arr[pos]);
+            }
+        }
+    }
+}
+
+
 //COUNT SORT
 void countSort(vector<int> &v, int mx, int mn)
 {
