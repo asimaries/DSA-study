b4b84bce3041b090c3b469cee6da2c6e41d00475
asimaries <logxtech59@gmail.com>
2022-01-07 15:39:59 +0530

    trees update

--- Trees/vertical_order_traversal.cpp
+++ Trees/vertical_order_traversal.cpp
@@ -2,18 +2,33 @@
 
 vector<vector<int>> verticalTraversal(Node *root)
 {
-    map <int,map <int, multiset <int>>> nodes;
-    queue <pair <Node*, pair <int, int>>> todo;
-    todo.push({root, {0,0}});
+    vector<vector<int>> ans;
+    if (root == NULL)
+        return ans;
+    map<int, map<int, multiset<int>>> mp;
+    queue<pair<Node *, pair<int, int>>> todo;
+    todo.push({root, {0, 0}});
     while (!todo.empty())
     {
-        auto p = todo.front();
+        auto curr = todo.front();
         todo.pop();
-        Node* curr = p.first;
-        int x= p.second.first, 
+        Node *node = curr.first;
+        int x = curr.second.first, y = curr.second.second;
+        mp[x][y].insert(node->val);
+        if (node->left)
+            todo.push({node->left, {x - 1, y + 1}});
+        if (node->right)
+            todo.push({node->right, {x + 1, y + 1}});
     }
-     
-    vector<vector<int>> ans;
+    for (auto p : mp)
+    {
+        vector<int> col;
+        for (auto q : p.second)
+            for (auto i : q.second)
+                col.emplace_back(i);
+
+        ans.push_back(col);
+    }
     return ans;
 }
 
@@ -25,11 +40,11 @@ int main()
     root1->right = new Node(3);
     root1->left->left = new Node(4);
     root1->left->right = new Node(5);
-    root1->left->right->left = new Node(6);
-    root1->right->left = new Node(7);
-    root1->right->right = new Node(8);
-    root1->right->right->left = new Node(9);
-    root1->right->right->right = new Node(10);
+    root1->right->left = new Node(6);
+    root1->right->right = new Node(7);
+    root1->left->right->left = new Node(8);
+    // root1->right->right->left = new Node(9);
+    // root1->right->right->right = new Node(10);
     // // root2
     // Node *root2 = new Node(70);
     // // root3
@@ -67,6 +82,6 @@ int main()
     // root->right->left->left = new Node(80);
     // root->right->left->right = new Node(90);
     // root->right->left->left->right = new Node(100);
-
+    printTree(root1);
     vector<vector<int>> ans = verticalTraversal(root1);
 }
